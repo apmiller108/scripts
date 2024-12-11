2ea5a76b51e024f8135d0b736ade59fa24016b7b
Alex Miller <apmiller108@yahoo.com>
2024-12-10 22:22:49 -0600

    Supports conversion to other types

--- convert-images.sh
+++ convert-images.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # Converts images in a directory to webp and resizes them. Places the converted images in a sub dir called "converted". 
-# arguments: 1) width 2) base filename and 3) path to images
-# Usage: ./convert-webp.sh 800 my-image path/to/dir
+# arguments: 1) width 2) base filename 3) extension 4) path to images
+# Usage: ./convert-images.sh 800 my-image path/to/dir
 
 # Check if correct number of arguments is provided
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0   "
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 WIDTH BASE_FILE_NAME EXTENSION DIRECTORY"
   exit 1
 fi
 
+
 WIDTH=$1
 BASE_FILENAME=$2
-DIRECTORY=$3
+EXT=$3
+DIRECTORY=$4
 
 # Check if directory exists
 if [ ! -d "$DIRECTORY" ]; then
@@ -30,7 +32,7 @@ COUNT=0
 for img in "$DIRECTORY"/*.{jpg,jpeg,png,JPG,JPEG,PNG}; do
   [ -f "$img" ] || continue
 
-  NEW_FILENAME="${BASE_FILENAME}_${COUNT}.webp"
+  NEW_FILENAME="${BASE_FILENAME}_${COUNT}.${EXT}"
 
   convert "$img" -resize "${WIDTH}x" "$CONVERTED_DIR/$NEW_FILENAME"
 
